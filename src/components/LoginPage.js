19c5189af4f960ea6929f33c46fb2e7413e3c918
kmarks2013 <kmarks2013@gmail.com>
2020-03-08 14:51:02 -0400

    initializes state

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 export default class LoginPage extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
     render() {
         return (
             <div>
