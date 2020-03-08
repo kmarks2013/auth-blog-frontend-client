f187ba8c4312a52225ea1614da2bcfc431e19536
kmarks2013 <kmarks2013@gmail.com>
2020-03-08 13:38:48 -0400

    adds loginpage

---
+++ src/components/LoginPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class LoginPage extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <label>Username: </label>
+                    <input type="text" name="username" />
+                    <label>Password: </label>
+                    <input type="text" name="password" />
+                    <input type="submit" />
+                </form>
+            </div>
+        )
+    }
+}
