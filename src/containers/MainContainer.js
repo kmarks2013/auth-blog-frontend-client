b69dc498604d40bedfb80946b8516bd814ec8a0c
kmarks2013 <kmarks2013@gmail.com>
2020-03-08 13:15:39 -0400

    adds main container component

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <div>
+                I will handle login
+            </div>
+        )
+    }
+}
+
+export default MainContainer
