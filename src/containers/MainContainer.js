0463618dcdb045a8b771898da04217040a5ed6e3
kmarks2013 <kmarks2013@gmail.com>
2020-03-08 13:39:16 -0400

    renders lgoin page in main container

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import LoginPage from '../components/LoginPage'
 
 class MainContainer extends Component {
     render() {
         return (
             <div>
-                I will handle login
+                <LoginPage />
             </div>
         )
     }
